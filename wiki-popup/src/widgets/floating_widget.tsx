e47e1c6daaf365afced6a05311ee6ea95a4faf2a
jamesb <experimentallearning0@gmail.com>
2022-08-26 11:04:10 +0100

    Update wiki-popup to latest API

--- wiki-popup/src/widgets/floating_widget.tsx
+++ wiki-popup/src/widgets/floating_widget.tsx
@@ -1,10 +1,10 @@
 import {WikiPopup} from "../components/wiki_popup";
-import {WidgetLocation, renderWidget, useRunAsync, usePlugin } from "@remnote/plugin-sdk";
+import {WidgetLocation, renderWidget, useRunAsync, usePlugin, RemId } from "@remnote/plugin-sdk";
 import {remIdKey} from "../lib/constants";
 
 function FloatingWikiPopup() {
   const plugin = usePlugin()
-  const remId = useRunAsync(async () => await plugin.storage.getSession(remIdKey), [])
+  const remId = useRunAsync(async () => await plugin.storage.getSession<RemId>(remIdKey), [])!
   return <WikiPopup remId={remId} location={WidgetLocation.FloatingWidget} />
 }
 
