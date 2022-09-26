09957d053e8bb17d74831d40e940483ed7df8236
Eikosa <darkofheking@gmail.com>
2022-09-26 16:46:01 +0300

    Remove from queue feature

--- hide-in-queue/src/widgets/index.tsx
+++ hide-in-queue/src/widgets/index.tsx
@@ -12,6 +12,14 @@ const CSS = `
   position: absolute;
   left: 25px;
 } 
+
+
+.rn-queue__content--answer-hidden [data-queue-rem-tags~="remove-from-queue"] .rn-rich-text{
+  opacity: 0;
+} 
+.rn-queue__content--answer-hidden [data-queue-rem-tags~="remove-from-queue"] .rn-bullet-container:after{
+  display: none;
+} 
 `;
 
 async function onActivate(plugin: ReactRNPlugin) {
@@ -19,6 +27,14 @@ async function onActivate(plugin: ReactRNPlugin) {
     "Hide in Queue",
     "hideInQueue",
     "Hides the tagged Rem in the queue view.",
+    {
+      slots: [],
+    }
+  );
+  await plugin.app.registerPowerup(
+    "Remove from Queue",
+    "removeFromQueue",
+    "Removes the tagged Rem in the queue view.",
     {
       slots: [],
     }
