daf5dec6740bf9aaaaa28048298d7a05f0077a37
jamesb <experimentallearning0@gmail.com>
2023-05-15 14:45:17 +0100

    misc

--- hide-in-queue/src/widgets/index.tsx
+++ hide-in-queue/src/widgets/index.tsx
@@ -8,17 +8,36 @@ import "../style.css";
 const CSS = `
 .rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .RichTextViewer{
   visibility: hidden;
+}
+
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-flashcard-delimiter {
+  visibility: hidden;
 } 
+
 .rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .RichTextViewer {
   visibility: hidden;
 } 
+
 .rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .rn-bullet-container:after{
   content: "Hidden in queue";
   opacity: .3;
   white-space: nowrap;
   position: absolute;
   left: 25px;
-} 
+}
+
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rem-bullet__document {
+  background-color: transparent !important;
+}
+
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rem-bullet__document:after {
+  content: "Hidden in queue";
+  opacity: .3;
+  white-space: nowrap;
+  position: absolute;
+  left: 25px;
+}
+
 .rn-queue__content--answer-hidden [data-queue-rem-container-tags~="remove-from-queue"]:not(.rn-question-rem) > .rn-queue-rem {
   display: none;
 }
