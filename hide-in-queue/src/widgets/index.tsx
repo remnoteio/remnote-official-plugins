88ccf33f48066880f9d3e331bb6f1892660d71f3
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2024-05-31 16:05:59 -0500

    🐛 fix(hidden-in-queue: css): rewrite element height for new content
    fixes issue with user (closes: ENG-25667)
    
    * A large block of hidden text in the queue was not using screen space efficiently
    * When the hidden queue text overlapped with a large block of wrapped text, it didn't free up screen space for other usable content, causing an inefficient use of screen real estate.

--- hide-in-queue/src/widgets/index.tsx
+++ hide-in-queue/src/widgets/index.tsx
@@ -1,62 +1,46 @@
 import {
-  declareIndexPlugin,
-  ReactRNPlugin,
-  SelectionType,
+	declareIndexPlugin,
+	ReactRNPlugin,
+	SelectionType,
 } from "@remnote/plugin-sdk";
 import "../style.css";
 
 const CSS = `
-.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .RichTextViewer{
-  opacity: 0;
-  visibility: hidden;
-}
-
-.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-flashcard-delimiter {
-  opacity: 0;
-  visibility: hidden;
-} 
-
-.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .RichTextViewer {
-  opacity: 0;
-  visibility: hidden;
-} 
-
-.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .rn-bullet-container:after{
-  content: "Hidden in queue";
-  opacity: .3;
-  white-space: nowrap;
-  position: absolute;
-  left: 25px;
-}
-
+/* General Hidden Styles */
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .RichTextViewer,
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-flashcard-delimiter,
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .RichTextViewer,
 .rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rem-bullet__document {
-  background-color: transparent !important;
+  display: none;
 }
 
-.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rem-bullet__document:after {
+/* Hidden in Queue Text */
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .rn-bullet-container,
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .rem-bullet__document {
+  position: relative;
+}
+
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .rn-bullet-container:after,
+.rn-queue__content--answer-hidden [data-queue-rem-container-tags~="hide-in-queue"]:not(.rn-question-rem) > .rn-queue-rem > .rem-bullet__document:after {
   content: "Hidden in queue";
   opacity: .3;
   white-space: nowrap;
   position: absolute;
   left: 25px;
+  top: 0;
 }
 
-.rn-breadcrumb-item[data-rem-tags~="hide-in-queue"] {
-  opacity: 0;
-}
-
+/* Remove from Queue Styles */
 .rn-queue__content [data-queue-rem-container-tags~="remove-from-queue"]:not(.rn-question-rem) > .rn-queue-rem {
   display: none;
 }
 
-.rn-queue__content [data-queue-rem-container-tags~="remove-from-queue"]:not(.rn-question-rem) {
-  margin-left: 0px !important;
-}
-
+.rn-queue__content [data-queue-rem-container-tags~="remove-from-queue"]:not(.rn-question-rem),
 .rn-breadcrumb-item[data-rem-tags~="remove-from-queue"] {
-  opacity: 0;
+  display: none;
 }
 
+/* No Hierarchy Styles */
 .rn-queue__content:has(.rn-question-rem[data-queue-rem-container-tags~="no-hierarchy"]) .indented-rem:not(.rn-question-rem) {
   margin-left: 0px !important;
 }
@@ -64,6 +48,7 @@ const CSS = `
 .rn-queue__content:has(.rn-question-rem[data-queue-rem-container-tags~="no-hierarchy"]) .indented-rem:not(.rn-question-rem) > .rn-queue-rem {
   display: none;
 }
+
 `;
 
 const HIDE_IN_QUEUE_POWERUP_CODE = "hideInQueue";
@@ -71,73 +56,73 @@ const REMOVE_FROM_QUEUE_POWERUP_CODE = "removeFromQueue";
 const NO_HIERARCHY_POWERUP_CODE = "noHierarchy";
 
 async function onActivate(plugin: ReactRNPlugin) {
-  await plugin.app.registerPowerup(
-    "No Hierarchy",
-    NO_HIERARCHY_POWERUP_CODE,
-    "Removes the ancestor hierarchy of the tagged Rem in the queue view.",
-    {
-      slots: [],
-    }
-  );
+	await plugin.app.registerPowerup(
+		"No Hierarchy",
+		NO_HIERARCHY_POWERUP_CODE,
+		"Removes the ancestor hierarchy of the tagged Rem in the queue view.",
+		{
+			slots: [],
+		}
+	);
 
-  const runAddPowerupCommand = async (powerup: string) => {
-    const sel = await plugin.editor.getSelection();
-    const selType = sel?.type;
-    if (!selType) {
-      return;
-    }
-    if (selType === SelectionType.Rem) {
-      const rems = (await plugin.rem.findMany(sel.remIds)) || [];
-      rems.forEach((r) => r.addPowerup(powerup));
-    } else {
-      const rem = await plugin.rem.findOne(sel.remId);
-      rem?.addPowerup(powerup);
-    }
-  };
+	const runAddPowerupCommand = async (powerup: string) => {
+		const sel = await plugin.editor.getSelection();
+		const selType = sel?.type;
+		if (!selType) {
+			return;
+		}
+		if (selType === SelectionType.Rem) {
+			const rems = (await plugin.rem.findMany(sel.remIds)) || [];
+			rems.forEach((r) => r.addPowerup(powerup));
+		} else {
+			const rem = await plugin.rem.findOne(sel.remId);
+			rem?.addPowerup(powerup);
+		}
+	};
 
-  await plugin.app.registerCommand({
-    id: `${NO_HIERARCHY_POWERUP_CODE}Cmd`,
-    name: "No Hierarchy",
-    action: async () => {
-      await runAddPowerupCommand(NO_HIERARCHY_POWERUP_CODE);
-    },
-  });
+	await plugin.app.registerCommand({
+		id: `${NO_HIERARCHY_POWERUP_CODE}Cmd`,
+		name: "No Hierarchy",
+		action: async () => {
+			await runAddPowerupCommand(NO_HIERARCHY_POWERUP_CODE);
+		},
+	});
 
-  await plugin.app.registerPowerup(
-    "Hide in Queue",
-    HIDE_IN_QUEUE_POWERUP_CODE,
-    "Hides the tagged Rem in the queue view.",
-    {
-      slots: [],
-    }
-  );
+	await plugin.app.registerPowerup(
+		"Hide in Queue",
+		HIDE_IN_QUEUE_POWERUP_CODE,
+		"Hides the tagged Rem in the queue view.",
+		{
+			slots: [],
+		}
+	);
 
-  await plugin.app.registerCommand({
-    id: `${HIDE_IN_QUEUE_POWERUP_CODE}Cmd`,
-    name: "Hide in Queue",
-    action: async () => {
-      await runAddPowerupCommand(HIDE_IN_QUEUE_POWERUP_CODE);
-    },
-  });
+	await plugin.app.registerCommand({
+		id: `${HIDE_IN_QUEUE_POWERUP_CODE}Cmd`,
+		name: "Hide in Queue",
+		action: async () => {
+			await runAddPowerupCommand(HIDE_IN_QUEUE_POWERUP_CODE);
+		},
+	});
 
-  await plugin.app.registerPowerup(
-    "Remove from Queue",
-    REMOVE_FROM_QUEUE_POWERUP_CODE,
-    "Removes the tagged Rem in the queue view.",
-    {
-      slots: [],
-    }
-  );
+	await plugin.app.registerPowerup(
+		"Remove from Queue",
+		REMOVE_FROM_QUEUE_POWERUP_CODE,
+		"Removes the tagged Rem in the queue view.",
+		{
+			slots: [],
+		}
+	);
 
-  await plugin.app.registerCommand({
-    id: `${REMOVE_FROM_QUEUE_POWERUP_CODE}Cmd`,
-    name: "Remove from Queue",
-    action: async () => {
-      await runAddPowerupCommand(REMOVE_FROM_QUEUE_POWERUP_CODE);
-    },
-  });
+	await plugin.app.registerCommand({
+		id: `${REMOVE_FROM_QUEUE_POWERUP_CODE}Cmd`,
+		name: "Remove from Queue",
+		action: async () => {
+			await runAddPowerupCommand(REMOVE_FROM_QUEUE_POWERUP_CODE);
+		},
+	});
 
-  await plugin.app.registerCSS("powerup", CSS);
+	await plugin.app.registerCSS("powerup", CSS);
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
