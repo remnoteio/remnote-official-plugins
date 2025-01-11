07c1c65398a20a577b0f17c232463bb9b1f41563
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2025-01-10 20:50:25 -0600

    Revert: Fix issues by internal adjustments within the app. (fixes #62)
    This reverts commit 73332e76611766d1ef4a249f10e0f7e317cff28a.

--- history/src/lib/utils.ts
+++ history/src/lib/utils.ts
@@ -1,49 +1,39 @@
 type IntervalType = "second" | "minute" | "hour" | "day" | "month" | "year";
 
-export function normalizeContent(content: any): string {
-	return Array.isArray(content)
-		? content
-				.map((item) =>
-					typeof item === "string" ? item : (item as any).text || ""
-				)
-				.join(" ")
-		: content;
-}
-
 export function timeSince(date: Date): string {
-	const seconds = Math.floor((+new Date() - +date) / 1000);
-	let intervalType: IntervalType;
-	let interval = Math.floor(seconds / 31536000);
-	if (interval >= 1) {
-		intervalType = "year";
-	} else {
-		interval = Math.floor(seconds / 2592000);
-		if (interval >= 1) {
-			intervalType = "month";
-		} else {
-			interval = Math.floor(seconds / 86400);
-			if (interval >= 1) {
-				intervalType = "day";
-			} else {
-				interval = Math.floor(seconds / 3600);
-				if (interval >= 1) {
-					intervalType = "hour";
-				} else {
-					interval = Math.floor(seconds / 60);
-					if (interval >= 1) {
-						intervalType = "minute";
-					} else {
-						interval = seconds;
-						intervalType = "second";
-					}
-				}
-			}
-		}
-	}
+  const seconds = Math.floor((+new Date() - +date) / 1000);
+  let intervalType: IntervalType;
+  let interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = "year";
+  } else {
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      intervalType = "month";
+    } else {
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = "day";
+      } else {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+          intervalType = "hour";
+        } else {
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = "minute";
+          } else {
+            interval = seconds;
+            intervalType = "second";
+          }
+        }
+      }
+    }
+  }
 
-	if (interval > 1 || interval === 0) {
-		intervalType += "s";
-	}
+  if (interval > 1 || interval === 0) {
+    intervalType += "s";
+  }
 
-	return interval + " " + intervalType + " ago";
+  return interval + " " + intervalType + " ago";
 }
